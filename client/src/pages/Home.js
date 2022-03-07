751ff3fa87cbb993d9cd28587fa2324cb0358e16
Greg Christiansen <gregchristiansen14@gmail.com>
2022-03-07 17:41:39 -0600

    profile user doesnt work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-import Auth from '../utils/auth';
+import ThoughtForm from '../components/ThoughtForm';
 
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
@@ -16,25 +17,34 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
           )}
         </div>
         {loggedIn && userData ? (
-        <div className="col-12 col-lg-3 mb-3">
-          <FriendList
-            username={userData.me.username}
-            friendCount={userData.me.friendCount}
-            friends={userData.me.friends}
-          />
-        </div>
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
         ) : null}
       </div>
     </main>
   );
 };
+
 
 export default Home;
